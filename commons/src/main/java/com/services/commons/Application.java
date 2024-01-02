50e20627d3599a54e769f24f2b1239c19baa568a
amo95 <jameaamo@gmail.com>
2024-01-02 21:05:37 +0000

    restructure api client module

--- commons/src/main/java/com/services/commons/Application.java
+++ commons/src/main/java/com/services/commons/Application.java
@@ -1,13 +1,13 @@
-package com.services.fetchrestaurantdata;
+package com.services.commons;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FetchrestaurantdataApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(FetchrestaurantdataApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
