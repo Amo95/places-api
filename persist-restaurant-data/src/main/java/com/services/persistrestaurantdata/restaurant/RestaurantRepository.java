4a08d5927b21c9cd81e9189f836f1fe74f1d0f50
amo95 <jameaamo@gmail.com>
2024-01-02 02:35:59 +0000

    changed JpaRepo type from String to Long

--- persist-restaurant-data/src/main/java/com/services/persistrestaurantdata/restaurant/RestaurantRepository.java
+++ persist-restaurant-data/src/main/java/com/services/persistrestaurantdata/restaurant/RestaurantRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RestaurantRepository extends JpaRepository<Restaurant, String> {
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 }
