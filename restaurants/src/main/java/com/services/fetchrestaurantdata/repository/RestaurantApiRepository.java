a0607789e0de05dd03170bfc6572da7b6951ecaf
amo95 <jameaamo@gmail.com>
2024-01-20 21:37:26 +0000

    created crud endpoints

--- restaurants/src/main/java/com/services/fetchrestaurantdata/repository/RestaurantApiRepository.java
+++ restaurants/src/main/java/com/services/fetchrestaurantdata/repository/RestaurantApiRepository.java
@@ -5,5 +5,4 @@ import com.services.fetchrestaurantdata.model.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RestaurantApiRepository extends JpaRepository<Restaurant, Long> {
-    Restaurant findByName(String name);
 }
--- restaurants/src/main/java/com/services/fetchrestaurantdata/service/Impl/IRestaurantData.java
+++ restaurants/src/main/java/com/services/fetchrestaurantdata/service/Impl/IRestaurantData.java
@@ -27,14 +27,25 @@ public class IRestaurantData implements RestaurantDataService {
     }
 
     @Override
-    public Restaurant getByName(String restaurantName) {
-        return restaurantRepository.findByName(restaurantName);
+    public Restaurant findRestaurant(Long id) {
+        return getRestaurant(id);
     }
 
     @Override
     public RestaurantResponse updateRestaurant(Long id, RestaurantRequest request) {
         Restaurant restaurant = basicMapper.convertTo(request, Restaurant.class);
         return basicMapper.convertTo(updateRestaurant(id, restaurant), RestaurantResponse.class);
+    }
+
+    @Override
+    public RestaurantResponse createRestaurant(RestaurantRequest request) {
+        Restaurant restaurant = basicMapper.convertTo(request, Restaurant.class);
+        return basicMapper.convertTo(addRestaurant(restaurant), RestaurantResponse.class);
+    }
+
+    @Override
+    public void removeRestaurant(Long restaurantId) {
+        restaurantRepository.deleteById(restaurantId);
     }
 
     private Restaurant getRestaurant(Long id) {
@@ -48,15 +59,15 @@ public class IRestaurantData implements RestaurantDataService {
         return restaurantRepository.save(restaurantData);
     }
 
-    @Override
-    public Restaurant createRestaurant(RestaurantRequest request) {
-        return null;
+    private Object addRestaurant(Restaurant restaurant) {
+
+        Restaurant restaurant1 = Restaurant.builder()
+                .name(restaurant.getName())
+                .rating(restaurant.getRating())
+                .working_time(restaurant.getWorking_time())
+                .phone_number(restaurant.getPhone_number())
+                .address(restaurant.getAddress())
+                .build();
+        return restaurantRepository.save(restaurant1);
     }
-
-    @Override
-    public Restaurant removeRestaurant(Long restaurantId) {
-        return null;
-    }
-
-
 }
